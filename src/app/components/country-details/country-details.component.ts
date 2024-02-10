d9ceb4548fc8106d81ea70f62cdb8c70bedb1069
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-10 15:08:20 +0000

    feat/ country details component

---
+++ src/app/components/country-details/country-details.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-country-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './country-details.component.html',
+  styleUrl: './country-details.component.css',
+})
+export class CountryDetailsComponent {}
