84fc85853c2aaf2a1428e7f258234e197ab4ba6e
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-10 15:10:48 +0000

    feat/ country card added to country list component

--- src/app/components/country-list/country-list.component.ts
+++ src/app/components/country-list/country-list.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { SearchBarComponent } from '../search-bar/search-bar.component';
 import { FilterComponent } from '../filter/filter.component';
+import { CountryCardComponent } from '../country-card/country-card.component';
 
 @Component({
   selector: 'app-country-list',
   standalone: true,
-  imports: [SearchBarComponent, FilterComponent],
+  imports: [SearchBarComponent, FilterComponent, CountryCardComponent],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.css',
 })
