3aa95bcc88496fdbb70364b193d565366e293ab5
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-10 15:04:37 +0000

    feat/ routes for navigation added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { CountryDetailsComponent } from './components/country-details/country-details.component';
+import { CountryListComponent } from './components/country-list/country-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/countries',
+    pathMatch: 'full',
+  },
+  {
+    path: 'countries',
+    component: CountryListComponent,
+  },
+  {
+    path: 'details/:name',
+    component: CountryDetailsComponent,
+  },
+];
