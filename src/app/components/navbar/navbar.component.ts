64d04d6cdd018fb338de35e9ac8164d2a218ac7f
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-12 11:42:35 +0000

    feat(app): implement dark/light theme toggle

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';``
+import { ThemeService } from '../../services/theme.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  isDarkTheme = false;
 
+  themeService = inject(ThemeService);
+
+  toggleTheme() {
+    this.isDarkTheme = !this.isDarkTheme;
+    this.themeService.toggleTheme();
+  }
 }
