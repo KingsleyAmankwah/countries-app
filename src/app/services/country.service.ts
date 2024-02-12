1f5130d18f93878a4c9b5df7326b6130b1148993
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-12 17:23:26 +0000

    feat/(app): Add search term handling in CountryService

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -2,14 +2,25 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { Country } from '../interface/country';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CountryService {
   apiUrl = environment.API_URL;
+
   http = inject(HttpClient);
+
+  private searchTerm = new BehaviorSubject<string>('');
+
+  updateSearchTerm(term: string) {
+    this.searchTerm.next(term);
+  }
+
+  getSearchTerm(): Observable<string> {
+    return this.searchTerm.asObservable();
+  }
 
   getCountries(): Observable<Country[]> {
     return this.http.get<Country[]>(this.apiUrl);
