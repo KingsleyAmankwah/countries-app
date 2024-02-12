2785766e9ffdd69da921a0aa8fa75d5519622709
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-12 17:28:28 +0000

    feat/(app): Implement search input handling

--- src/app/components/search-bar/search-bar.component.ts
+++ src/app/components/search-bar/search-bar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
+import { CountryService } from '../../services/country.service';
 
 @Component({
   selector: 'app-search-bar',
@@ -13,6 +14,12 @@ export class SearchBarComponent {
   isDarkTheme = false;
 
   themeService = inject(ThemeService);
+  countryService = inject(CountryService);
+
+  handleInputChange(event: Event) {
+    const inputValue = (event.target as HTMLInputElement).value;
+    this.countryService.updateSearchTerm(inputValue);
+  }
 
   ngOnInit() {
     this.themeService.isDarkTheme.subscribe((darkTheme) => {
