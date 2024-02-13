f31f6ba1353a77b1deac71b347cc4b8587dfc885
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-13 18:25:35 +0000

    feat/(app): Loading border functionality out

--- src/app/components/country-details/country-details.component.ts
+++ src/app/components/country-details/country-details.component.ts
@@ -1,7 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { ThemeService } from '../../services/theme.service';
+import { CountryService } from '../../services/country.service';
+import { Country } from '../../interface/country';
 
 @Component({
   selector: 'app-country-details',
@@ -12,12 +14,39 @@ import { ThemeService } from '../../services/theme.service';
 })
 export class CountryDetailsComponent {
   isDarkTheme = false;
-
+  isLoading = true;
+  countryDetails!: Country[];
   themeService = inject(ThemeService);
+  countryService = inject(CountryService);
+
+  route = inject(ActivatedRoute);
 
   ngOnInit() {
     this.themeService.isDarkTheme.subscribe((darkTheme) => {
       this.isDarkTheme = darkTheme;
     });
+
+    this.route.params.subscribe((params) => {
+      const countryName = params['name'];
+      this.countryService
+        .getCountryDetails(countryName)
+        .subscribe((country) => {
+          this.isLoading = false;
+          this.countryDetails = [country[0]];
+          console.log(this.countryDetails);
+        });
+    });
+  }
+
+  // loadBorderCountry(border: string) {
+  //   // Fetch the details of the border country and update the `country` variable
+  //   // This is just a placeholder, replace it with your actual code to fetch the country details
+  //   this.countryService.getCountryDetails(border).subscribe((country) => {
+  //     this.countryDetails = country;
+  //   });
+  // }
+
+  objectKeys(obj: { [key: string]: {} }): string[] {
+    return Object.keys(obj);
   }
 }
