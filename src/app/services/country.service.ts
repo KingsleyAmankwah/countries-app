016d6e4361a3633f2c2e7034b08ba373a8e46662
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-13 11:12:14 +0000

    feat/(app): Enhanced code readability and consistency in CountryService

--- src/app/services/country.service.ts
+++ src/app/services/country.service.ts
@@ -8,21 +8,23 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CountryService {
-  apiUrl = environment.API_URL;
+  BASE_URL = environment.API_URL;
 
   http = inject(HttpClient);
 
   private searchTerm = new BehaviorSubject<string>('');
+  currentSearchTerm = this.searchTerm.asObservable();
 
   updateSearchTerm(term: string) {
     this.searchTerm.next(term);
   }
 
-  getSearchTerm(): Observable<string> {
-    return this.searchTerm.asObservable();
+  getCountries(): Observable<Country[]> {
+    return this.http.get<Country[]>(this.BASE_URL + 'all');
   }
 
-  getCountries(): Observable<Country[]> {
-    return this.http.get<Country[]>(this.apiUrl);
+  getCountryDetails(name: string): Observable<Country[]> {
+    const url = `${this.BASE_URL}name/${name}`;
+    return this.http.get<Country[]>(url);
   }
 }
