55e100d3406020e0854439813b8b6c37ad132ae2
Kingsley Amankwah <kingsley.amankwah@amalitech.com>
2024-02-15 07:57:29 +0000

    refactor/(app): backtits removed from core modules

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';``
+import { Component, inject } from '@angular/core';
 import { ThemeService } from '../../services/theme.service';
 import { CommonModule } from '@angular/common';
 
